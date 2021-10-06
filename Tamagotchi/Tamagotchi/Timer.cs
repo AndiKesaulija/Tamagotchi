6af2a540a86907eb397ec6001391e08ee1da8b17
Andi Kesaulija <a.kesaulija@hotmail.com>
2021-10-06 21:24:32 +0200

    Add project files.

---
+++ Tamagotchi/Tamagotchi/Timer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Timers;
+
+namespace Tamagotchi
+{ 
+    public static class Timer
+    {
+        private static System.Timers.Timer aTimer;
+
+        public delegate void TimeEvents(object obj, EventArgs e);
+        public static TimeEvents timeEvents;
+
+        public static void Main()
+        {
+            SetTimer();
+
+
+            //aTimer.Stop();
+            //aTimer.Dispose();
+
+        }
+        private static void SetTimer()
+        {
+            
+            Console.WriteLine("Timer Started");
+            // Create a timer with a two second interval.
+            aTimer = new System.Timers.Timer(2000);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            timeEvents(source, e);
+            Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
+                              e.SignalTime);
+        }
+
+        
+    }
+
+
+
+
+
+}
---
+++ Tamagotchi/Tamagotchi/Views/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tamagotchi.Views
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public int statHunger = 1000;
+        public MainPage()
+        {
+            InitializeComponent();
+            Timer.Main();
+            Timer.timeEvents += Decrease_Hunger;
+            //Hunger.Text = statHunger.ToString();
+
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PopUp(object sender, EventArgs e)
+        {
+            //Popup_Stats.IsVisible = !Popup_Stats.IsVisible;
+        }
+
+        private void Add_Hunger(object sender, EventArgs e)
+        {
+            statHunger = 1000;
+            //Hunger.Text = statHunger.ToString();
+
+            Console.WriteLine("Add_Hunger: " + statHunger);
+        }
+
+        private void Decrease_Hunger(object sender, EventArgs e)
+        {
+            statHunger = statHunger - 10;
+            //Hunger.Text = statHunger.ToString();
+
+            Console.WriteLine("Decrease_Hunger: " + statHunger);
+        }
+        private void Push_Actions(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Actions());
+        }
+
+
+
+
+    }
+}
